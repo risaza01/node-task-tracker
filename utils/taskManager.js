ca209c588d4c92fa254a4ce0c6d47cc2443f9ae1
risaza01 <risaza0102@gmail.com>
2025-06-01 01:53:29 -0500

    Se agregó la funcionalidad de obtener todas las tareas del archivo

--- utils/taskManager.js
+++ utils/taskManager.js
@@ -20,6 +20,37 @@ class TaskManager {
       }
     })();
   }
+
+  async getTasks() {
+    try {
+      let fileHandle = await fs.open("tasks.json", "r");
+      let stream = fileHandle.createReadStream();
+      let data = "";
+
+      // Se retorna una promesa ya que los métodos del stream son asíncronos, por lo que queremos que retornen una respuesta al terminar la lectura
+      return new Promise((resolve, reject) => {
+        stream.on("data", (chunk) => {
+          data += chunk.toString("utf8");
+        });
+
+        stream.on("end", async () => {
+          await fileHandle.close();
+          try {
+            resolve(JSON.parse(data));
+          } catch (err) {
+            reject(new Error("Error al parsear JSON: " + err.message));
+          }
+        });
+
+        stream.on("error", async (err) => {
+          await fileHandle.close();
+          reject(new Error("Error de lectura: " + err.message));
+        });
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 module.exports = TaskManager;
